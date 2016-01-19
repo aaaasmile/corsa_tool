fd741e3847c456e51af652f4b544f1d8e21380a6
aaaasmile <testhaus@gmx.at>
2016-01-19 16:53:40 +0100

    Vertical scrolling

--- RunnerTool/main.js
+++ RunnerTool/main.js
@@ -1,5 +1,6 @@
 ﻿chrome.app.runtime.onLaunched.addListener(function () {
     chrome.app.window.create('index.html', {
+        id: "RunCorsaTool",
         'outerBounds': {
             'width': 1200,
             'height': 800
